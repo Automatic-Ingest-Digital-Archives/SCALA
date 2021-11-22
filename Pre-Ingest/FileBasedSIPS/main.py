30b7621fdd384fb8fe5f67364c717bbb28b1b810
JelleKleevensVAI <87436774+JelleKleevensVAI@users.noreply.github.com>
2021-11-22 12:56:40 +0100

    Update main.py

--- Pre-Ingest/FileBasedSIPS/main.py
+++ Pre-Ingest/FileBasedSIPS/main.py
@@ -6,40 +6,46 @@ from xmlgen_file import createXmlString
 from pathlib import Path
 
 """ sys.argv[0] = script name
-    sys.argv[1] = source dir = dir containing Transfer Sets
+    sys.argv[1] = source dir
     sys.argv[2] = output dir
     sys.argv[3] = xml description metadata output dir
+    sys.argv[4] = delete original after copying to new dir TRUE/FALSE
 """
 
 rootDir = sys.argv[1]
 outputDir = sys.argv[2]
 xmlOutputDir = sys.argv[3]
+deleteOriginal = sys.argv[4].lower() == "true"
 
-for transferSet in next(os.walk(rootDir))[1]:
-    """0. Create Transfer Set dir in outputDir."""
-    tsOutputDir = os.path.join(outputDir, transferSet)
-    Path(tsOutputDir).mkdir(parents=True, exist_ok=True)
-    transferSetDir = os.path.join(rootDir, transferSet)
+"""0. Create Transfer Set dir in outputDir."""
+tsFolderName = os.path.basename(rootDir)
+tsOutputDir = os.path.join(outputDir, tsFolderName)
+Path(tsOutputDir).mkdir(parents=True, exist_ok=True)
 
-    for d, _dirs, files in os.walk(transferSetDir):
-        for f in files:
-            """1. Prepare variables + make new folder in tsOutputDir."""
-            relDir = os.path.relpath(d, transferSetDir)
-            filename = os.path.splitext(f)[0]
-            fileSipTargetDirName = f"{filename}-{str(uuid.uuid4())}"
-            fileSipTargetDir = os.path.join(tsOutputDir, fileSipTargetDirName)
-            Path(fileSipTargetDir).mkdir(parents=True, exist_ok=True)
+for d, _dirs, files in os.walk(rootDir):
+    for f in files:
+        """1. Prepare variables + make new folder in tsOutputDir."""
+        relDir = os.path.relpath(d, rootDir)
+        filename = os.path.splitext(f)[0]
+        fileSipTargetDirName = f"{filename}-{str(uuid.uuid4())}"
+        fileSipTargetDir = os.path.join(tsOutputDir, fileSipTargetDirName)
+        Path(fileSipTargetDir).mkdir(parents=True, exist_ok=True)
 
-            """2. Copy file in targetDir."""
+        """2. Copy file in targetDir. Optionally delete rootDir."""
+        try:
             sourceFile = os.path.join(d, f)
             shutil.copy(sourceFile, fileSipTargetDir)
+            if deleteOriginal == True:
+                os.rmdir(rootDir)
+        except Exception as Argument:
+            print(Argument)
 
-            """3. Make description xml and put in xmlOutputDir."""
-            relFilePath = os.path.join(relDir, f).replace("\\", "/")
-            xml = createXmlString(relFilePath, filename)
-            xmlSavePath = os.path.join(xmlOutputDir, f"{fileSipTargetDirName}.xml")
-            try:
-                with open(xmlSavePath, 'w', encoding='UTF-8') as file:
-                    file.write(xml)
-            except Exception as Argument:
-                print(Argument)
+        """3. Make description xml and put in xmlOutputDir."""
+        relFilePath = os.path.join(relDir, f).replace("\\", "/")
+        xml = createXmlString(relFilePath, filename)
+        xmlSavePath = os.path.join(xmlOutputDir, f"{fileSipTargetDirName}.xml")
+        try:
+            with open(xmlSavePath, 'w', encoding='UTF-8') as file:
+                file.write(xml)
+        except Exception as Argument:
+            print(Argument)
