8111077303782b6c19c6bd3d4ff1c027c1e8956e
Jelle Kleevens <jelle.kleevens@vai.be>
2021-12-28 12:09:46 +0100

    contain dir loop in script

--- Pre-Ingest/FileBasedSIPS/main.py
+++ Pre-Ingest/FileBasedSIPS/main.py
@@ -6,7 +6,7 @@ from xmlgen_file import createXmlString
 from pathlib import Path
 
 """ sys.argv[0] = script name
-    sys.argv[1] = source dir
+    sys.argv[1] = source dir (dir containing transfer sets)
     sys.argv[2] = output dir
     sys.argv[3] = xml description metadata output dir
     sys.argv[4] = delete original after copying to new dir TRUE/FALSE
@@ -17,35 +17,43 @@ outputDir = sys.argv[2]
 xmlOutputDir = sys.argv[3]
 deleteOriginal = sys.argv[4].lower() == "true"
 
-"""0. Create Transfer Set dir in outputDir."""
-tsFolderName = os.path.basename(rootDir)
-tsOutputDir = os.path.join(outputDir, tsFolderName)
-Path(tsOutputDir).mkdir(parents=True, exist_ok=True)
+# rootDir = r"C:\Users\VAIJelleKleevens\OneDrive - Vlaams Architectuurinstituut vzw\Bureaublad"
+# outputDir = r"C:\Users\VAIJelleKleevens\OneDrive - Vlaams Architectuurinstituut vzw\Bureaublad\testoutput"
+# xmlOutputDir = r"C:\Users\VAIJelleKleevens\OneDrive - Vlaams Architectuurinstituut vzw\Bureaublad\xmltestoutput"
+# deleteOriginal = False
 
-for d, _dirs, files in os.walk(rootDir):
-    for f in files:
-        """1. Prepare variables + make new folder in tsOutputDir."""
-        relDir = os.path.relpath(d, rootDir)
-        filenameWithExtension = os.path.basename(f)
-        fileSipTargetDirName = f"{filenameWithExtension}-{str(uuid.uuid4())}"
-        fileSipTargetDir = os.path.join(tsOutputDir, fileSipTargetDirName)
-        Path(fileSipTargetDir).mkdir(parents=True, exist_ok=True)
+for transferSet in next(os.walk(rootDir))[1]:
+    """0. Create Transfer Set dir in outputDir."""
+    print(transferSet)
+    tsOutputDir = os.path.join(outputDir, transferSet)
+    Path(tsOutputDir).mkdir(parents=True, exist_ok=True)
+    transferSetDir = os.path.join(rootDir, transferSet)
+    tsFolderName = transferSet
 
-        """2. Copy file in targetDir. Optionally delete rootDir."""
-        try:
-            sourceFile = os.path.join(d, f)
-            shutil.copy(sourceFile, fileSipTargetDir)
-            if deleteOriginal == True:
-                os.rmdir(rootDir)
-        except Exception as Argument:
-            print(Argument)
+    for d, _dirs, files in os.walk(transferSetDir):
+        for f in files:
+            """1. Prepare variables + make new folder in tsOutputDir."""
+            relDir = os.path.relpath(d, transferSetDir)
+            filenameWithExtension = os.path.basename(f)
+            fileSipTargetDirName = f"{filenameWithExtension}-{str(uuid.uuid4())}"
+            fileSipTargetDir = os.path.join(tsOutputDir, fileSipTargetDirName)
+            Path(fileSipTargetDir).mkdir(parents=True, exist_ok=True)
 
-        """3. Make description xml and put in xmlOutputDir."""
-        relFilePath = os.path.join(relDir, f).replace("\\", "/")
-        xml = createXmlString(relFilePath, filenameWithExtension, tsFolderName)
-        xmlSavePath = os.path.join(xmlOutputDir, f"{fileSipTargetDirName}.xml")
-        try:
-            with open(xmlSavePath, 'w', encoding='UTF-8') as file:
-                file.write(xml)
-        except Exception as Argument:
-            print(Argument)
+            """2. Copy file in targetDir. Optionally delete rootDir."""
+            try:
+                sourceFile = os.path.join(d, f)
+                shutil.copy(sourceFile, fileSipTargetDir)
+                if deleteOriginal == True:
+                    os.rmdir(rootDir)
+            except Exception as Argument:
+                print(Argument)
+
+            """3. Make description xml and put in xmlOutputDir."""
+            relFilePath = os.path.join(relDir, f).replace("\\", "/")
+            xml = createXmlString(relFilePath, filenameWithExtension, tsFolderName)
+            xmlSavePath = os.path.join(xmlOutputDir, f"{fileSipTargetDirName}.xml")
+            try:
+                with open(xmlSavePath, 'w', encoding='UTF-8') as file:
+                    file.write(xml)
+            except Exception as Argument:
+                print(Argument)
