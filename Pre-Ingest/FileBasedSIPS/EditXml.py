b48900de3bc90bd8426edcf46b5672e9beadca11
JelleKleevensVAI <87436774+JelleKleevensVAI@users.noreply.github.com>
2022-01-12 15:55:36 +0100

    Update EditXml.py

--- Pre-Ingest/FileBasedSIPS/EditXml.py
+++ Pre-Ingest/FileBasedSIPS/EditXml.py
@@ -10,30 +10,33 @@ rootDir = sys.argv[1]
 # rootDir = r"C:\Users\VAIJelleKleevens\OneDrive - Vlaams Architectuurinstituut vzw\Bureaublad\unzipeditrezip\zips"
 
 def updateXml(originalXml):
-    tree = ET.parse(originalXml)
-    root = tree.getroot()
+    try:
+        tree = ET.parse(originalXml)
+        root = tree.getroot()
 
-    """Tree changes here:"""
-    
-    scopeContents = root.findall(".//scopecontent")
-    for scopeContent in scopeContents:
-        text = scopeContent.text
-        scopeContent.text = ""
-        p = ET.SubElement(scopeContent, "p")
-        p.text = text
+        """Tree changes here:"""
         
-    scopeContents = root.findall(".//{ns0}scopecontent")
-    for scopeContent in scopeContents:
-        text = scopeContent.text
-        scopeContent.text = ""
-        p = ET.SubElement(scopeContent, "{ns0}p")
-        p.text = text
+        scopeContents = root.findall(".//scopecontent")
+        for scopeContent in scopeContents:
+            text = scopeContent.text
+            scopeContent.text = ""
+            p = ET.SubElement(scopeContent, "p")
+            p.text = text
+            
+        scopeContents = root.findall(".//{ns0}scopecontent")
+        for scopeContent in scopeContents:
+            text = scopeContent.text
+            scopeContent.text = ""
+            p = ET.SubElement(scopeContent, "{ns0}p")
+            p.text = text
 
-    """"""
+        """"""
 
-    xmlstr = ET.tostring(root, encoding="UTF-8", xml_declaration=True).decode("UTF-8")
-    with open(originalXml, 'w', encoding="UTF-8") as f:
-        f.write(xmlstr)
+        xmlstr = ET.tostring(root, encoding="UTF-8", xml_declaration=True).decode("UTF-8")
+        with open(originalXml, 'w', encoding="UTF-8") as f:
+            f.write(xmlstr)
+    except Exception as Argument:
+        print(Argument)
 
 for _rootDirs, _dirs, files in os.walk(rootDir):
     for f in files:
